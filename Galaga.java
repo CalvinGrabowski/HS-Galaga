b594d2099be856648c4c4b60ce2e9cc586e595fe
Calvin Grabowski <calvingrabowski@gmail.com>
2026-01-16 15:26:38 -0800

    Add README screenshots and update .gitignore

--- Galaga.java
+++ Galaga.java
@@ -3,25 +3,26 @@
 
 import javax.swing.JFrame;
 import java.awt.*;
-        
+
 public class Galaga {
-   
+
    public static void main(String[] args) {
-   
-      JFrame window = new JFrame();          //makes a window that will hold the display panel
+
+      JFrame window = new JFrame(); // makes a window that will hold the display panel
       window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-      window.setResizable(false);            //makes the display usable
+      window.setResizable(false); // makes the display usable
       window.setTitle("Galaga");
-      
-      Display panel = new Display();         //creates the display panel
-      
-      window.add(panel);                     //makes the display panel a window
+
+      Display panel = new Display(); // creates the display panel
+
+      window.add(panel); // makes the display panel a window
       window.pack();
-      
-      window.setLocationRelativeTo(null);    //the window is created in the middle of the screen
-      window.setVisible(true);               //this makes the window and does everything that makes it the size and look that we want
-      
-      panel.startGameThread();               //starts the game
+
+      window.setLocationRelativeTo(null); // the window is created in the middle of the screen
+      window.setVisible(true); // this makes the window and does everything that makes it the size and look
+                               // that we want
+
+      panel.startGameThread(); // starts the game
    }
 
 }
